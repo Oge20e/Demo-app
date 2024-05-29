701edcf08665db51ba78316e316dd49e7bb8070a
Ogechi <163291656+Oge20e@users.noreply.github.com>
2024-05-29 09:27:19 +0100

    first commit

---
+++ src/main/java/com/hm/confrence_demo/ConfrenceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hm.confrence_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfrenceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfrenceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hm/confrence_demo/ConfrenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.confrence_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfrenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
